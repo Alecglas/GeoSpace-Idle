cbf75219bb37da474151e6d7b1edaf495864ca63
Jeffrey Kerley <jeffreykerley@mwc-031245.dhcp.missouri.edu>
2021-11-06 13:03:12 -0500

    finished homepage

--- home.js
+++ home.js
@@ -1,63 +1,112 @@
 let width;
 let height;
 let svg;
-let radius;
+let radius = 50;
+let x1 = 0;
+let y1 = 0;
+let sliderX = [];
+let sliderY = [];
+let currentSlide = 0;
+const circleAmount = 300;
+let circleNumb = [];
+let resetX;
+let resetY;
+let slideAmount = .5;
+let slideIter = .01;
+
+
     document.addEventListener("DOMContentLoaded", function(){
         width = window.innerWidth;
-
         height = window.innerHeight;
-        console.log(width,height);
-        
+        const ps = document.getElementsByTagName("p");
+        let counter = 10;
+        ps.forEach(element => {
+          element.style.left = ""+counter+"px";
+          element.style.top = "-25%";
+          counter = counter + 120;
+        });
+        createSlider();
     });
   function goTo(page){
     window.location.href = page;
   }
   function setup() {
+    console.log("works");
     createCanvas(width, height);
+     
   }
-  
   function draw() {
     //background(102);
-    
     if (mouseIsPressed) {
-      background(0);
-    } else {
       background(255);
+      circle(width/2,height/2,radius);
+      fill(255);
+      radius+=30;
+      if(radius>width+300){
+        goTo("space/space.html");
+      }
+    } else {
+      background(0);
+      
     }
-    let currentX = .06;
-    let currentY = .5;
-    let currentP = 2;
-    let currentR = 30;
-    for(let x = 0; x < 20; x++){
-      push();
-      translate(width * currentX, height * currentY);
-      rotate(frameCount / -200.0);
-      polygon(0, 0, currentR, currentP);
-      pop();
-      currentP = currentP + 1;
-      // currentR = currentR + 5;
-      currentX = currentX + .05;
+    let points = [0,10,10,0];
+    
+    for(let x = 0; x < 220; x++){
+      line(points[0],points[1],points[2],points[3]);
+      stroke(100);
+      points[1] += 10;
+      points[2] += 10;
+    }
+    for (let zz = 0; zz < circleAmount; zz++) {
+      circle(sliderX[zz],sliderY[zz],8);
+       sliderX[zz] += slideAmount;
+       sliderY[zz] -= slideAmount;
+       slideAmount += slideIter;
 
+      if(zz%15 ===1){
+        slideIter = slideIter*-1;
+      }
+      
+      stroke(255);    
+      fill("black");
     }
-    push();
-    translate(width * 0.0, height * 0.5);
-    //rotate(frameCount / 200.0);
-    polygon(0, 0, 30, 1);
-    pop();
-  
-    // push();
-    // translate(width * 0.5, height * 0.5);
-    // rotate(frameCount / 50.0);
-    // polygon(0, 0, 80, 20);
-    // pop();
-  
-    // push();
-    // translate(width * 0.8, height * 0.5);
-    // rotate(frameCount / -100.0);
-    // polygon(0, 0, 70, 7);
-    // pop();
+    slideAmount = .1;
+
+    currentSlide = random(circleNumb);
+    // if(currentSlide>150){
+    //   sliderY[currentSlide] += Math.floor(random(-325,0));
+    //   sliderX[currentSlide] += Math.floor(random(-325,0));
+    //   resetX = width;
+    //   resetY = height;
+    // }else{
+    //   sliderY[currentSlide] += Math.floor(random(0,325));
+    //   sliderX[currentSlide] += Math.floor(random(0,325));
+    //   resetX = 0;
+    //   resetY = 0;
+    // }
+    // if((sliderY[currentSlide] && sliderX[currentSlide]) > 2000 || (sliderY[currentSlide] && sliderX[currentSlide]) <0){
+    //   sliderY[currentSlide] = 0;
+    //   sliderX[currentSlide] = 0;
+    // }
+    stroke(50);
   }
-  
+
+  function createSlider(){
+    console.log("works");
+    for (let xx = 0; xx < circleAmount; xx++) {
+      sliderX.push(x1);
+      sliderY.push(y1);
+      circleNumb.push(xx);
+      if(xx==circleAmount/2 ){
+        x1 = width/2;
+        y1 = 0;
+        console.log("fart");
+      }
+       x1+=10;
+       y1+=10;
+    }
+  }
+
   function polygon(x, y, radius, npoints) {
     let angle = TWO_PI / npoints;
     beginShape();
